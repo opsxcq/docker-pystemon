f43e69be6f196beafcd8b090136b58547ff56275
OPSXCQ <opsxcq@strm.sh>
2016-11-29 18:46:55 -0200

    main fix

--- main.sh
+++ main.sh
@@ -10,13 +10,6 @@ do
                 renice -n 20 $pid 2>&1 > /dev/null 
         done; 
 
-        # Kill if it's running for more than an hour (CPU TIME)
-        for pid in $(ps -def | grep pyst | grep -v '00:' | awk '{print $2}')
-        do
-                kill $pid 2>&1 > /dev/null
-                echo $(date) " - killed pystemon"
-        done;
-
         for pid in $(ps -def | grep curl | awk '{print $2}')
         do 
                 renice -n 20 $pid 2>&1 > /dev/null
