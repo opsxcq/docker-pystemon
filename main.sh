d2754885b72cd353df6d06378c114b375f70e688
OPSXCQ <opsxcq@strm.sh>
2016-12-11 22:01:21 -0200

    main refactor

--- main.sh
+++ main.sh
@@ -1,27 +1,37 @@
 #!/bin/bash
 
-cd /pystemon
-python pystemon.py&
 
-while true
-do 
+function priorities(){
+    while true
+    do 
         # Renice for pystemon
-        for pid in $(ps -def | grep pyst | awk '{print $2}')
+        for pid in $(ps -def | grep pyst | grep -v grep | awk '{print $2}')
         do 
-                renice -n 20 $pid 2>&1 > /dev/null 
+            renice -n 20 $pid 2>&1 > /dev/null 
         done; 
 
-        for pid in $(ps -def | grep curl | awk '{print $2}')
+        for pid in $(ps -def | grep curl | grep -v grep |  awk '{print $2}')
         do 
-                renice -n 20 $pid 2>&1 > /dev/null
+            renice -n 20 $pid 2>&1 > /dev/null
         done; 
 
         # Kill if it's running for more than an hour (CPU TIME)
-        for pid in $(ps -def | grep curl | grep -v '00:00' | awk '{print $2}')
+        for pid in $(ps -def | grep curl | grep -v '00:00' | grep -v grep | awk '{print $2}')
         do
-                kill $pid 2>&1 > /dev/null
-                echo $(date) " - Killed curl"
+            kill $pid 2>&1 > /dev/null
+            echo $(date) " - Killed curl"
         done;
 
         sleep 10; 
-done;
+    done;
+}
+
+priorities&
+
+while true
+do
+    python pystemon.py
+    echo '[-] Pystemon died'
+    sleep 5
+done
+
